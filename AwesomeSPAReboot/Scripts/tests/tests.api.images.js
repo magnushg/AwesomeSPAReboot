afdf685f3cf8d91953fd47ff6dd5d1dd2d16054e
magnushg <magnus.h.green@gmail.com>
2013-01-18 13:47:32 +0100

    Added api test files

---
+++ AwesomeSPAReboot/Scripts/tests/tests.api.images.js
@@ -0,0 +1,11 @@
+﻿///<reference path="tests.config.js"/>
+
+require(['ajaxhelper'], function (ajax) {
+    test("images api returns data", function () {
+        var result;
+        ajax.ajaxRequest("api/images").done(function (data) {
+            result = data;
+            ok(true, result.length > 0);
+        });
+    });
+});
